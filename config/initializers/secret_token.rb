ddda24b063139b282a116ff028d994ceb7f1c12f
mmmoore13 <mmmoore13@gmail.com>
2014-03-21 13:55:57 -0400

    Created a Rails App with Status Updates: 1)rails new (name)App,rails g scaffold status name:string content:text, rake db:migrate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnelasttryApp::Application.config.secret_key_base = '55569226287a792d4cbd5907eb2e2014086910ab7de64adf06a3ce5cbe45d116ea2b6abc710c32f767e91e9b3aaa9153198757f3f160ae7b93b832b590c155d9'
